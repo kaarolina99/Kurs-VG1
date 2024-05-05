47c1e335714e99ad5d768fe428144628a94338a4
kaarolina99 <kapaa008@osloskolen.no>
2024-05-05 20:36:10 +0200

    ferdig sid stegonografi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import image from "./questionablebildepåentomnettside.png"
+</script>
+
+
+<h1>Ingenting her</h1>
+<div>
+    <img alt="lol" src={image}>
+</div>
+
